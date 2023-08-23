76159d6bfc2919e8663f443e2d5fe6c48a1fd0b2
Tulio-Rangel <tjrn20@hotmail.com>
2023-08-23 15:30:46 -0500

    add response template file & finish init structure

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const response = require("../../../network/response");
+
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.send("Everything is fine");
+  response.success(req, res, "Todo bien", 200);
 });
 
 module.exports = router;
