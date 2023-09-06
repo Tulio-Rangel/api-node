53addb90cd56b834cb0aea06e2947d6eab9853dc
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-06 17:37:21 -0500

    document user with swagger

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
 
 const config = require("../config.js");
 const user = require("./components/user/network.js");
@@ -8,8 +9,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const swaggerDoc = require("./swagger.json");
+
 // Router
 app.use("/api/user", user);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   console.log(`API listening on port ${config.api.port}`);
