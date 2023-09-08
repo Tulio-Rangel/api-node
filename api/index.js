8e7018b97cb25e127cf07aaf928d1f07bcd85be7
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-08 14:40:49 -0500

    making login a generate JWT

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const swaggerUi = require("swagger-ui-express");
 
 const config = require("../config.js");
 const user = require("./components/user/network.js");
+const auth = require("./components/auth/network.js");
 
 const app = express();
 
@@ -13,6 +14,7 @@ const swaggerDoc = require("./swagger.json");
 
 // Router
 app.use("/api/user", user);
+app.use("/api/auth", auth);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
